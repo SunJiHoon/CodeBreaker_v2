08809540e980de1c2d0fed9f4aa28e8933168154
SunJiHoon <cangogi1@naver.com>
2024-06-28 19:52:23 +0900

    sytleTransfer의 do Transfer 주석 추가 제작

--- CodeBreaker/styleTransfer/styleTransfer.py
+++ CodeBreaker/styleTransfer/styleTransfer.py
@@ -3,38 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow_hub as hub
 
-# �̹����� �ε��ϰ� ��ó���ϴ� �Լ�
+# 이미지를 로드하고 전처리하는 함수
 def load_image(image_path):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_image(image, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.image.resize(image, [224, 224])  # �̹��� ũ�� ����
-    image = image[tf.newaxis, :]
+    image = tf.io.read_file(image_path)  # 이미지 파일을 읽어옵니다.
+    image = tf.image.decode_image(image, channels=3) #binary 이미지를 3차원으로 디코딩
+    image = tf.image.convert_image_dtype(image, tf.float32) # 이미지 데이터를 float32 형식으로 변환합니다.
+    image = tf.image.resize(image, [224, 224])   # 이미지 크기를 224x224로 조정합니다.
+    image = image[tf.newaxis, :] # 배치 차원을 추가합니다.
     return image
 
 
 def doTransfer():
-    # �̹��� ��� ����
-    content_image_path = 'styleTransfer/content_image_5.jpg'  # ��� �̹���
-    style_image_path = 'styleTransfer/style_image_5.jpg'      # ��Ÿ�� �̹���
+    # 이미지 경로 설정
+    content_image_path = 'styleTransfer/content_image_5.jpg'
+    style_image_path = 'styleTransfer/style_image_5.jpg'
     
-    # �̹����� �ε��ϰ� ��ó��
+    # 이미지를 로드하고 전처리합니다.
     content_image = load_image(content_image_path)
     style_image = load_image(style_image_path)
     
-    # TF-Hub���� ���� �Ʒõ� ��Ÿ�� Ʈ������ �� �ε�
+    # TF-Hub에서 Magenta 스타일 전송 모델을 로드합니다.
     hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
     
-    # ��Ÿ�� Ʈ������ ����
+    # 스타일 transfer을 수행합니다.
     stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
     stylized_image = tf.squeeze(stylized_image)  # ���� ���
     
-    # ��� �̹��� ���
     plt.imshow(stylized_image)
     plt.axis('off')
     plt.show()
-    
-    # ��� �̹��� ����
+    # 결과 이미지를 저장합니다.
     tf.keras.utils.save_img('styleTransfer/result_image.jpg', stylized_image.numpy())
     
 def doTransferwithUserImage():
