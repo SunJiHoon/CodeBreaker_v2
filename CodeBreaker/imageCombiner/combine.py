2920e280f083137f64ebb7f8de60a6823d8a4beb
SunJiHoon <cangogi1@naver.com>
2024-07-05 18:54:00 +0900

    현수막 배경 변경

--- CodeBreaker/imageCombiner/combine.py
+++ CodeBreaker/imageCombiner/combine.py
@@ -4,19 +4,24 @@ import textwrap
 
 def drawImagewithTextDescriptiveAboutOpenAndSave(strList, openAt, saveAt):
     # 원본 이미지 불러오기
-    # style_image = Image.open('styleTransfer/styled_result_image1.jpg')
     style_image = Image.open(openAt)
 
-    # 흰색 배경 이미지 생성 (300x1500)
+
+    # # 흰색 배경 이미지 생성 (300x1500)
     background_width = 1500
     background_height = 300
-    background = Image.new('RGB', (background_width, background_height), 'white')
+    # background = Image.new('RGB', (background_width, background_height), 'white')
+
+    # 흰색 배경 이미지 불러오기
+    background = Image.open("./userImage/userBackgroundImage.jpg")
+
+    # 배경 이미지 크기를 1500x300으로 조정
+    background = background.resize((background_width, background_height))
 
     # 스타일 이미지 크기
     style_image_width, style_image_height = style_image.size
 
     # 스타일 이미지 위치 (좌측 중앙에 배치)
-    # position = (0, (background_height - style_image_height) // 2)
     position = (10, 10)
     background.paste(style_image, position)
 
@@ -38,10 +43,8 @@ def drawImagewithTextDescriptiveAboutOpenAndSave(strList, openAt, saveAt):
     text_width = text_bbox[2] - text_bbox[0]
     text_height = text_bbox[3] - text_bbox[1]
     text_position = (style_image_width + 50, (background_height - text_height) // 2)
-    # draw.text(text_position, text, fill='black', font=font)  # 흰색 배경이므로 검은색 텍스트 사용
     draw.multiline_text(text_position, wrapped_text, fill='black', font=font)  # 흰색 배경이므로 검은색 텍스트 사용
     # 결과 이미지를 파일로 저장
-    # background.save('newCoverPage/result1.jpg')
     background.save(saveAt)
 
 def drawImagewithText(strList):
