9f2510dbdad79d464df8c0fdeadbed2b00f4f29a
SunJiHoon <cangogi1@naver.com>
2024-07-11 08:59:48 +0900

    스타일 트랜스퍼 고자ㅓㅇ 직전 userPicture를 가져올 때 해상도 하 현상 이슈 해결

--- CodeBreaker/styleTransfer/getUserPicture.py
+++ CodeBreaker/styleTransfer/getUserPicture.py
@@ -3,8 +3,8 @@ import cv2
 # from getImage import projection
 
 def getUserImage(img_result):
-    width = 100*5  # to be resized
-    height = 141*5  
+    width = 100*5 * 5  # to be resized
+    height = 141*5 * 5
     resized_image = cv2.resize(img_result, (width, height))
     # resized_image = projection.resize(img_result, height, width)
     
@@ -12,11 +12,15 @@ def getUserImage(img_result):
     #cv2.imshow("resized_Image", resized_image)
     #cv2.waitKey()
     #cv2.destroyAllWindows()
-    x = 77  
-    y = 274
-    width = 248
-    height = 330 
-    cropped_image = resized_image[y:y+height, x:x+width]
+    x = 77   * 5
+    y = 274 * 5
+    # width = 248
+    # height = 330
+    x_fin = (77 + 248) * 5
+    y_fin = (274 + 330) * 5
+    # cropped_image = resized_image[y:y + height, x:x + width]
+    cropped_image = resized_image[y:y_fin, x:x_fin]
+
     #cv2.imshow("cropped_image_stylebefore", cropped_image)
     #cv2.imshow("resized_Image", resized_image)
     #cv2.waitKey()
